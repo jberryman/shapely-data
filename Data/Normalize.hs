5a30bd76c5b2e5529a3a6a46ed184a9ca8f27e57
Brandon Simmons <brandon.m.simmons@gmail.com>
2012-03-03 19:52:44 -0500

    fromNorm compiles

--- Data/Normalize.hs
+++ Data/Normalize.hs
@@ -28,9 +28,8 @@ class Normable a b | a -> b, b -> a where
  -         Right ()   --> Right ()
  -                    --> Nothing
  -
- -     This I guess means that above, we should have:
-            class Normable a b | a -> b where
-       ...since the normed form doesn't actually 
+       ambiguity in our class conversions is avoided since each has a newtype
+       wrapper
 
        TODO:
        - finish code generation
@@ -48,7 +47,7 @@ class Normable a b | a -> b, b -> a where
 -- This requires the @TemplateHaskell@ extension to be enabled.
 mkNormable :: Name -> Q [Dec]
 mkNormable n = 
-    do (TyConI d) <- reify n
+    do (TyConI d) <- reify n  -- what about PrimTyconI? should we represent literals as newtype-wrapped literals?
        let (DataD cxt nm bndings cnstrctrs derivng) = d
 
        -- --------------------------------------------------------
@@ -60,8 +59,9 @@ mkNormable n =
 
        -- --------------------------------------------------------
        -- build the Normable class instance for this type
+       frmClauses <- fromNormClauses cnstrctrs
        let toNormDec = FunD 'toNorm (toNormClauses cnstrctrs)
-           fromNormDec = undefined
+           fromNormDec = FunD 'fromNorm frmClauses
        let normableInstance = InstanceD [] normableTs [toNormDec, fromNormDec]
            normableTs = (ConT ''Normable) `AppT` (decToType d) `AppT` (decToType wrapper)
        return [wrapper,normableInstance]
@@ -81,28 +81,31 @@ bndNames (KindedTV n _) = n
 
 
 -- takes list of constructors to CONVERT TO, pattern matching against Either, (), (,)
-fromNormClauses :: [Con] -> [Clause]
-fromNormClauses cs = zipWith (\p bdy-> Clause [p] bdy []) (fromNormPats cs) (map fromNormBdy cs)
+--fromNormClauses :: [Con] -> [Clause]
+--fromNormClauses cs = zipWith (\p bdy-> Clause [p] bdy []) (fromNormPats cs) (map fromNormBdy cs)
+fromNormClauses :: [Con] -> Q [Clause]
+fromNormClauses cs = do bdies <- mapM fromNormBdy cs
+                        let pats = fromNormPats cs
+                        return $ zipWith (\p bdy-> Clause [p] bdy []) pats bdies
 
 -- for each constructor, return the pattern to match 
 fromNormPats :: [Con] -> [Pat]
-fromNormPats [_] = [sumVar]
-fromNormPats (_:cs) = ConP 'Left sumPat : (map wrapRightPat $ fromNormPats cs)
+fromNormPats [_] = [sumPat]
+fromNormPats (_:cs) = ConP 'Left [sumPat] : (map wrapRightPat $ fromNormPats cs)
 fromNormPats [] = error "type has no constructors"
 
 sumPat = VarP $ mkName "sumVar"
+sumExp = VarE $ mkName "sumVar"
 wrapRightPat p = ConP 'Right [p] 
 
 -- given pattern match above, return a body that de-tuples args into Con
-fromNormBdy :: Con -> Body
-fromNormBdy (NormalC nm sts) = NormalB $ deTuple $ length sts
-    where deTuple :: Int -> Exp -- function applying n-kind constructor to n-nested tuples
-          -- THIS ISN'T RIGHT: Left () is Con
-          --                   Left a is Con a
+fromNormBdy :: Con -> Q Body
+fromNormBdy (NormalC nm sts) = fmap NormalB $ deTuple $ length sts
+    where deTuple :: Int -> Q Exp -- function applying n-kind constructor to n-nested tuples
           deTuple 0 = return (ConE nm) -- empty constructor (e.g. Nothing) 
-          deTuple n = [| $(deTupleN n) $(ConE nm) sumVar |] -- 'sumVar' is our bound tuple above
+          deTuple n = [| $(deTupleN n) $(return $ ConE nm) $(return sumExp) |] -- 'sumVar' is our bound tuple above
           deTupleN 1 = [| \constr a-> constr a |]
-          deTupleN n = [| \constr (a,b)-> $(deTupleN (n-1) (constr a) b)
+          deTupleN n = [| \constr (a,b)-> $(deTupleN (n-1)) (constr a) b |]
 
 -- takes a list of constructors to CONVERT FROM (pattern match against)
 toNormClauses :: [Con] -> [Clause]
