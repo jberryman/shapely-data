3a65e95afe45cd9e8583ef3f9fc2926065eec0eb
Brandon Simmons <brandon.m.simmons@gmail.com>
2013-12-21 18:04:16 -0500

    GHC.Generics example recreated

--- examples/Generics.hs
+++ examples/Generics.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE DefaultSignatures, DeriveGeneric, TypeOperators, FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell, TypeFamilies #-}
+module Main
+    where
  
-import GHC.Generics
+import Data.Shapely
 import Data.Bits
 
 -- This example copied from: 
@@ -13,51 +16,39 @@ data Bit = O | I deriving Show
 class Serialize a where
   put :: a -> [Bit]
  
-  default put :: (Generic a, GSerialize (Rep a)) => a -> [Bit]
+  default put :: (Shapely a, GSerialize (Normal a)) => a -> [Bit]
   put a = gput (from a)
  
   get :: [Bit] -> (a, [Bit])
  
-  default get :: (Generic a, GSerialize (Rep a)) => [Bit] -> (a, [Bit])
+  default get :: (Shapely a, GSerialize (Normal a)) => [Bit] -> (a, [Bit])
   get xs = (to x, xs')
     where (x, xs') = gget xs
  
 class GSerialize f where
-  gput :: f a -> [Bit]
-  gget :: [Bit] -> (f a, [Bit])
+  gput :: f -> [Bit]
+  gget :: [Bit] -> (f, [Bit])
  
 -- | Unit: used for constructors without arguments
-instance GSerialize U1 where
-  gput U1 = []
-  gget xs = (U1, xs)
+instance GSerialize () where
+  gput () = []
+  gget xs = ((), xs)
  
 -- | Constants, additional parameters and recursion of kind *
-instance (GSerialize a, GSerialize b) => GSerialize (a :*: b) where
-  gput (a :*: b) = gput a ++ gput b
-  gget xs = (a :*: b, xs'')
-    where (a, xs') = gget xs
+instance (Serialize a, GSerialize b) => GSerialize (a , b) where
+  gput (a , b) = put a ++ gput b
+  gget xs = ((a , b), xs'')
+    where (a, xs') = get xs
           (b, xs'') = gget xs'
  
 -- | Meta-information (constructor names, etc.)
-instance (GSerialize a, GSerialize b) => GSerialize (a :+: b) where
-  gput (L1 x) = O : gput x
-  gput (R1 x) = I : gput x
-  gget (O:xs) = (L1 x, xs')
+instance (GSerialize a, GSerialize b) => GSerialize (Either a b) where
+  gput (Left x) = O : gput x
+  gput (Right x) = I : gput x
+  gget (O:xs) = (Left x, xs')
     where (x, xs') = gget xs
-  gget (I:xs) = (R1 x, xs')
+  gget (I:xs) = (Right x, xs')
     where (x, xs') = gget xs
- 
--- | Sums: encode choice between constructors
-instance (GSerialize a) => GSerialize (M1 i c a) where
-  gput (M1 x) = gput x
-  gget xs = (M1 x, xs')
-    where (x, xs') = gget xs
- 
--- | Products: encode multiple arguments to constructors
-instance (Serialize a) => GSerialize (K1 i a) where
-  gput (K1 x) = put x
-  gget xs = (K1 x, xs')
-    where (x, xs') = get xs
  
 instance Serialize Bool where
   put True = [I]
@@ -70,7 +61,8 @@ instance Serialize Bool where
 --
  
 data UserTree a = Node a (UserTree a) (UserTree a) | Leaf
-  deriving (Generic, Show)
+  deriving (Show)
+$(deriveShapely ''UserTree)
  
 instance (Serialize a) => Serialize (UserTree a)
  
